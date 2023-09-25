9b09eca8ae8529554c7b1b33b7e5c2cb2f894ce4
Runek00 <runek00@gmail.com>
2023-09-26 01:35:37 +0200

    Interactivity, baby!

--- task.go
+++ task.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 var nextTaskId int = 0
 var taskStore map[int]Task = make(map[int]Task)
@@ -30,13 +36,74 @@ func showTasks() {
 }
 
 func completeTask(id int) {
-	t := taskStore[id]
+	t, ok := taskStore[id]
+	if !ok {
+		fmt.Print("No such task")
+		return
+	}
 	t.completed = true
 	taskStore[id] = t
 }
 
 func deleteTask(id int) {
+	_, ok := taskStore[id]
+	if !ok {
+		fmt.Print("No such task")
+		return
+	}
 	delete(taskStore, id)
+}
+
+func interactive() {
+	for {
+		fmt.Print("\nWhat do you want to do?\n(add) add task\n(del) delete task\n(com) complete task\n(show) show tasks\n(exit) exit\n")
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+		switch strings.TrimSpace(input) {
+		case "add":
+			addTask(getTitle())
+		case "del":
+			deleteTask(getId())
+		case "com":
+			completeTask(getId())
+		case "show":
+			showTasks()
+		case "exit":
+			fmt.Print("BAIII!\n")
+			return
+		default:
+			fmt.Print("No such option\n")
+		}
+	}
+}
+
+func getTitle() string {
+	fmt.Print("\nWhat is the title?\n")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	return strings.TrimSpace(input)
+}
+
+func getId() int {
+	fmt.Print("\nWhat is the id?\n")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	inn, err := strconv.Atoi(strings.TrimSpace(input))
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	return inn
 }
 
 func main() {
@@ -49,4 +116,5 @@ func main() {
 	completeTask((3))
 	deleteTask(4)
 	showTasks()
+	interactive()
 }
