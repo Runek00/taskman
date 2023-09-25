87cfee743a14f4260385b99bd904949d4c1ebcc5
Runek00 <runek00@gmail.com>
2023-09-25 23:25:58 +0200

    create tasks

--- task.go
+++ task.go
@@ -1,7 +1,20 @@
 package main
 
+var idSeq int = 0
+var taskStore []Task = make([]Task, 0)
+
 type Task struct {
 	id        int
 	title     string
 	completed bool
+}
+
+func createTask(title string) Task {
+	out := Task{idSeq, title, false}
+	idSeq++
+	return out
+}
+
+func addTask(title string) {
+	taskStore = append(taskStore, createTask(title))
 }
